33596a23c7b8a0a4dac9efd2bb8f6ab0155bddf2
syakimovich <s0yakimovich@gmail.com>
2016-02-19 12:11:10 +0300

    Gini measure

---
+++ src/main/java/org/yakimovich/evolca/measures/Gini.java
@@ -0,0 +1,35 @@
+package org.yakimovich.evolca.measures;
+
+import org.yakimovich.evolca.Measure;
+import org.yakimovich.evolca.Universe;
+
+public class Gini implements Measure {
+
+    @Override
+    public String getName() {
+        return "Gini";
+    }
+
+    @Override
+    public double getValue(Universe u) {
+        int numberOfStates = u.getNumberOfStates();
+        double[] numberArray = new double[numberOfStates];
+        for(int i = 0; i < numberOfStates; i++){
+            numberArray[i] = 0;
+        }
+
+        char[][] cells = u.getCells();
+        for(int i = 0; i < u.getWidth(); i++) {
+            for (int j = 0; j < u.getHeight(); j++) {
+                numberArray[cells[i][j]]++;
+            }
+        }
+        double absDiffSum = 0;
+        for(int i = 0; i < numberOfStates; i++){
+            for(int j = 0; j < numberOfStates; j++){
+                absDiffSum += Math.abs(numberArray[i] - numberArray[j]);
+            }
+        }
+        return 100.0 * absDiffSum / (double) ((numberOfStates - 1.0) * u.getWidth() * u.getHeight() * 2.0);
+    }
+}
