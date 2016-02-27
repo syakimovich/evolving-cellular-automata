576538d1cfd8475f08523c31e4b75ea348e7304b
syakimovich <s0yakimovich@gmail.com>
2016-02-27 10:09:28 +0300

    unified Universum random example

--- src/main/java/org/yakimovich/evolca/example/universesum/RandomExample.java
+++ src/main/java/org/yakimovich/evolca/example/universesum/RandomExample.java
@@ -10,17 +10,15 @@ import org.yakimovich.evolca.utils.InitialStateGenerator;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 
-public class MoreZeros {
-
-	public static void main(String[] args) throws InterruptedException {
-        char size = 100;
-        char numberOfStates = 64;
+public class RandomExample {
+    public static void main(String[] args){
+        int size = 100;
+        char numberOfStates = 8;
         int numberOfNeighbors = 4;
-        double initialStateToZeroProbability = 0;
+        boolean isCircular = true;
+        String initialState = "";
         double resultStateToZeroProbability = 0.8;
-        Random r = new Random();
 
         if(args != null && args.length >= 1){
             numberOfStates = (char) Integer.parseInt(args[0]);
@@ -28,35 +26,41 @@ public class MoreZeros {
             if(args.length >= 2){
                 numberOfNeighbors = (char) Integer.parseInt(args[1]);
             }
+
+            if(args.length >= 3){
+                initialState = args[2];
+            }
+
+            if(args.length >= 4){
+                resultStateToZeroProbability = Double.parseDouble(args[3]);
+            }
         }
-
-        final boolean isCircular = true;
-
-
-
 
         List<Universe> universes = new ArrayList<Universe>();
         for(int i = 0; i < 10; i++){
-            char[][] initialCells = InitialStateGenerator.createRandom(size, size, numberOfStates);
-            initialCells = ArrayUtils.replaceWithZeros(initialCells, initialStateToZeroProbability);
-
+            char[][] initialCells = null;
+            if(initialState.equalsIgnoreCase("point")){
+                initialCells = InitialStateGenerator.create1Point(size, size, (char)1);
+            } else if(initialState.equalsIgnoreCase("rectangular")){
+                initialCells = InitialStateGenerator.create3ColorsRectangular(size, size);
+            } else {
+                initialCells = InitialStateGenerator.createRandom(size, size, numberOfStates);
+            }
             int[][] thresholds = ArrayUtils.createRandom2DIntArray(numberOfStates, numberOfStates - 1,
                     (numberOfStates - 1) * numberOfNeighbors);
             char[][] resultStates = ArrayUtils.createRandom2DCharArray(numberOfStates,numberOfStates, numberOfStates);
             resultStates = ArrayUtils.replaceWithZeros(resultStates, resultStateToZeroProbability);
-
-            resultStates[0][0] = 0;
-
+            for(int j = 0; j < resultStates.length; j++){
+                resultStates[j][0] = 0;
+            }
             Universe u = new UniverseSum(initialCells, numberOfNeighbors, thresholds, resultStates, numberOfStates, isCircular);
             universes.add(u);
         }
 
-        MainWindow mainWindow = new MainWindow("Simple random example");
+        MainWindow mainWindow = new MainWindow("Random example");
         mainWindow.addMeasure(new AvgNeighborColorIndex5());
         mainWindow.addMeasure(new Gini());
         mainWindow.setSleepTimeInMilliseconds(100);
         mainWindow.setUniverses(universes);
-
-	}
-
+    }
 }
